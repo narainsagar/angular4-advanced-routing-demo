8c4c81831f82661a389de3abef0220a120e2cd9a
Narain Sagar <narainmenghwar@gmail.com>
2017-10-11 08:37:36 +0500

    build --prod fix

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
-import {FlightSearchComponent} from './flight-search/flight-search.component';
-import {FlightCardComponent} from './flight-card/flight.card.component';
-import {AltFlightCardComponent} from './alt-flight-card/alt-flight.card.component';
-import {FlightListComponent} from './alt-flight-card/flight-list';
-import {PassengerSearchComponent} from './passenger-search/passenger-search.component';
-import {FlightEditComponent} from './flight-edit/flight-edit.component';
-import {FlightSearchReactiveComponent} from './flight-search-reactive/flight-search-reactive.component';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SharedModule} from '../shared/shared.module';
-import {FlightBookingRouterModule} from './flight-booking.routes';
-import {FlightBookingComponent} from './flight-booking.component';
-import {FlightService} from './services/flight.service';
-import {FlightResolver} from './services/flight.resolver';
-import {FlightHistoryComponent} from '../flight-history/flight-history.component';
-import {ChatComponent} from './chat/chat.component';
+import { NgModule } from '@angular/core';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightCardComponent } from './flight-card/flight.card.component';
+import { AltFlightCardComponent } from './alt-flight-card/alt-flight.card.component';
+import { FlightListComponent } from './alt-flight-card/flight-list';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightSearchReactiveComponent } from './flight-search-reactive/flight-search-reactive.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { FlightBookingRouterModule } from './flight-booking.routes';
+import { FlightBookingComponent } from './flight-booking.component';
+import { FlightService } from './services/flight.service';
+import { FlightResolver } from './services/flight.resolver';
+import { FlightHistoryComponent } from '../flight-history/flight-history.component';
+import { ChatComponent } from './chat/chat.component';
 import { AuthModule } from '../auth/auth.module';
 
 
@@ -36,7 +36,8 @@ import { AuthModule } from '../auth/auth.module';
         FlightSearchReactiveComponent,
         PassengerSearchComponent,
         FlightEditComponent,
-        FlightBookingComponent
+        FlightBookingComponent,
+        ChatComponent // TODO: remove
     ],
     providers: [
         FlightService,
